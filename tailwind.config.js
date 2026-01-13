b637f8ffff9fa9a180544226e16f763b3ca8e5ad
gallomdg-svg <gallomdg@gmail.com>
2026-01-13 19:06:32 -0300

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cala: "#c4161c", // rojo Sergio Cala
+      },
+    },
   },
   plugins: [],
 };
